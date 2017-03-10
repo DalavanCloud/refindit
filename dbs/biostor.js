87a308b4bd19e28a1dd0dfaa4026e626b87322ea
Roderic Page <r.page@bio.gla.ac.uk>
2017-03-10 22:54:36 +0000

    Better processing of BioStor data

--- dbs/biostor.js
+++ dbs/biostor.js
@@ -45,21 +45,54 @@ function biostorParse(body) {
 			title:			ref.reference.title,
 			year:			ref.reference.year,
 			
-			publishedIn:		!empty(ref.reference.journal) ? ref.reference.journal.name : undefined,
-			volume:				!empty(ref.reference.journal.volume) ? ref.reference.journal.volume : undefined,
-			issue:				!empty(ref.reference.journal.issue) ? ref.reference.journal.issue : undefined,
-
-			//volume:			ref.volume,
-			//issue:			ref.number,
+			publishedIn:	!empty(ref.reference.journal) ? ref.reference.journal.name : undefined,
+			volume:			!empty(ref.reference.journal.volume) ? ref.reference.journal.volume : undefined,
+			issue:			!empty(ref.reference.journal.issue) ? ref.reference.journal.issue : undefined,
 			
-			//firstauthor:	authors[0] || [],
+			spage:			!empty(ref.reference.journal.pages) ? starting_page(ref.reference.journal.pages) : undefined,
+			epage:			!empty(ref.reference.journal.pages) ? ending_page(ref.reference.journal.pages) : undefined,
+			
+			firstauthor:	authors[0] || [],
 			isParsed:		true,
 			score:			ref.score,
 			
 			fullCitation:	ref.hit,
-			type:			refTypes[(ref.reference.type || "").toLowerCase()]
+			type:			refTypes[(ref.reference.type || "").toLowerCase()],
+			
+			href:			ref.id.replace(/biostor\//, 'http://biostor.org/reference/'),
+			
+			doi:			get_doi(ref.reference.identifier)
 		};
 	}, 'BioStor::References');
 }
+
+function starting_page(p) {
+	var spage = undefined;
+	var delimiter = p.indexOf('-');
+	if (delimiter != -1) {
+	  spage = p.substring(0, delimiter);
+	}
+	return spage;
+}
+
+function ending_page(p) {
+	var epage = undefined;
+	var delimiter = p.indexOf('-');
+	if (delimiter != -1) {
+	  epage = p.substring(delimiter+2);
+	} 
+	return epage;
+}
+
+function get_doi(identifier) {
+	var doi = undefined;
+	for (var i in identifier) {
+		if (identifier[i].type == 'doi') {
+			doi = identifier[i].id;
+		}
+	}
+	return doi;
+}
+
 
 function empty(sth) { return typeof sth === 'undefined' || sth === "" || sth === null; }
