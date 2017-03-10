de61418f0464aada83cedd0e2d18fae6eebbdfb9
Roderic Page <r.page@bio.gla.ac.uk>
2017-03-10 18:31:07 +0000

    Determine protocol dynamically
    Based on
    https://github.com/Biserkov/refindit/commit/fcc9d6c2b8e6e716716e692adb4e
    e9ae176163ac

--- client/client.js
+++ client/client.js
@@ -321,12 +321,7 @@ function doSearch(type, trigger) {
 			$("#results").html('');
 			oReq.addEventListener("load",      allDBsReady, false);
 			oReq.addEventListener("progress", someDBsReady, false);
-			var api_call = 'http://' + window.location.host  +'/find?' + query;
-			
-			api_call += '&db=biostor';
-			console.log(api_call );
-			
-			
+			var api_call = window.location.protocol + '//' + window.location.host  +'/find?' + query;
 			$('#queries').show();
 			$('#api').attr('href', api_call);
 			oReq.open("get", api_call + '&more=1', true);
@@ -342,7 +337,7 @@ function callFormattingService(dropdown) {
 	var textarea = $('#styles_chosen').next();
 	var oReq  = new XMLHttpRequest();
 		oReq.onload = function(){ textarea.html(this.responseText); };
-		var server = 'http://' + window.location.host;
+		var server = window.location.protocol + '//' + window.location.host;
 		oReq.open("get", server +'/format?ref=' + ref + '&style=' + style, true);
 		oReq.send();
 	return false;
